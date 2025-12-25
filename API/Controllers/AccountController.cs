50446e1e8fbc764d692957efdc2c5390ddaf71b0
soroush.jj00 <soroush.jj00@gmail.com>
2025-12-25 19:00:40 +0100

    create register Method in AccountController and add RegisterDto

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace API.Controllers
+{
+    public class AccountController(AppDbContext context) : BaseApiController
+    {
+        [HttpPost("register")] //  api/account/register
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) 
+        {
+            if(await EmailExists(registerDto.Email))
+                return BadRequest("Email taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
+        }
+    }
+}
